05a5d11aa243d387b8265d0612a8d68600b38473
parvez-mosharof <toparvez16@gmail.com>
2026-03-30 00:04:05 +0600

    Updated project files

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 
 <div class="page-wrapper">
 
-        <section class="hero">
-            <div class="container hero-grid">
+    <section class="hero">
+        <div class="container hero-grid">
 
-                <div class="hero-text">
-                    <h2>Online English Learning Made Easy. Learn from Anywhere.</h2>
-                    <p>You can improve your English skills through structured online courses, interactive exercises, and multimedia resources that cover reading, writing, listening, speaking, grammar, and vocabulary.</p>
-                </div>
+            <div class="hero-text">
+                <h2>Online English Learning Made Easy. Learn from Anywhere.</h2>
+                <p>You can improve your English skills through structured online courses, interactive exercises, and multimedia resources that cover reading, writing, listening, speaking, grammar, and vocabulary.</p>
+            </div>
 
-                <div class="hero-visual">
-    <div class="code-box">
-<pre>
+            <div class="hero-visual">
+                <div class="code-box">
+                    <pre>
 <span class="comment">// EngFormation Lesson</span>
 
 <span class="keyword">Lesson:</span> Daily English Practice
@@ -26,13 +26,13 @@
 
 <span class="keyword">Goal:</span>
 <span class="string">Speak confidently in real life.</span>
-</pre>
-        <span class="cursor">|</span>
-    </div>
-</div>
-
+                    </pre>
+                    <span class="cursor">|</span>
+                </div>
             </div>
-        </section>
+
+        </div>
+    </section>
 
 </div>
 
